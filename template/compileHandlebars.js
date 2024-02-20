0bc666a9e53aaf234c830d08e854627b90cd5054
Damiano Plebani <damiano.plebani@pagopa.it>
2024-02-20 16:00:04 +0100

    Add modular `handlebars` partials

--- template/compileHandlebars.js
+++ template/compileHandlebars.js
@@ -15,8 +15,46 @@ Handlebars.registerHelper("lowercase", lowercase);
 // Import partials
 const partialPath = `./partials`;
 const header = fs.readFileSync(`${partialPath}/header.hbs`, "utf8");
+const noticeMainInfo = fs.readFileSync(
+  `${partialPath}/notice-main-info.hbs`,
+  "utf8"
+);
+const noticePaymentInfo = fs.readFileSync(
+  `${partialPath}/notice-payment-info.hbs`,
+  "utf8"
+);
+const noticePaymentMethods = fs.readFileSync(
+  `${partialPath}/notice-payment-methods.hbs`,
+  "utf8"
+);
+const noticePaymentData = fs.readFileSync(
+  `${partialPath}/notice-payment-data.hbs`,
+  "utf8"
+);
+const noticePaymentPoste = fs.readFileSync(
+  `${partialPath}/notice-payment-poste.hbs`,
+  "utf8"
+);
+// Common partials
+const noticePaymentQR = fs.readFileSync(
+  `${partialPath}/notice-payment-qrcode.hbs`,
+  "utf8"
+);
+const noticePaymentBollettino = fs.readFileSync(
+  `${partialPath}/notice-payment-bollettino.hbs`,
+  "utf8"
+);
+
 // Register partials
 Handlebars.registerPartial("header", header);
+Handlebars.registerPartial("noticeMainInfo", noticeMainInfo);
+Handlebars.registerPartial("noticePaymentInfo", noticePaymentInfo);
+Handlebars.registerPartial("noticePaymentMethods", noticePaymentMethods);
+Handlebars.registerPartial("noticePaymentData", noticePaymentData);
+Handlebars.registerPartial("noticePaymentPoste", noticePaymentPoste);
+//-- Commons
+Handlebars.registerPartial("noticePaymentQR", noticePaymentQR);
+Handlebars.registerPartial("noticePaymentBollettino", noticePaymentBollettino);
 
 const templateFile = fs.readFileSync("template.hbs", "utf8");
 const template = Handlebars.compile(templateFile);
