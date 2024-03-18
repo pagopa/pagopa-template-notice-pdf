49ed0c9a4ba1385dc05c03e9b25c57215961b21e
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-03-18 15:50:05 +0100

    qrCode tests

---
+++ template/helpers/genQrCode.js
@@ -0,0 +1,21 @@
+var QRCode = require("qrcode-svg");
+
+function genQrCode(data) {
+  var qrcode = new QRCode({
+    content: "http://github.com/",
+    padding: 0,
+    width: 256,
+    height: 256,
+    color: "#000000",
+    background: "#ffffff",
+    ecl: "M",
+    join: true
+  });
+  qrcode.save("sample.svg", function(error) {
+    if (error) throw error;
+    console.log("Done!");
+  });
+  return "sample.svg";
+}
+
+module.exports = genQrCode;
