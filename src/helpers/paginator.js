069586426a4d8867db4043bed9b2c828052e6503
Damiano Plebani <damiano.plebani@pagopa.it>
2024-06-12 16:02:51 +0200

    Slightly improve comments, code formatting and syntax

--- src/helpers/paginator.js
+++ src/helpers/paginator.js
@@ -1,18 +1,21 @@
-/* Inputs:
+/**
 
-notice: the notice with instalments: from 2 to 9 items
-withBollettinoPoste: boolean to indicate if in the same page there is also the bollettino postale
+Inputs:
+- Notice:
+    Object with instalments sub-array, from 2 to 9 items
+    (+9 items are managed with another template)
+- withBollettinoPoste
+    Boolean value indicating the presence/absence of the "Bollettino Postale"
 
 Outputs:
-create an n-dimensional array to divide the instalments in pages and rows.
+- Create an n-dimensional array to divide the instalments in pages and rows.
 
-Example:
+EXAMPLE:
 
 instalments [1,2,3,4,5,6,7]
 withBollettinoPoste: false
 
------
-
+Output:
 [
          page1        page2
 row1  [ [1,2,3] ] , [ [4,5] ]
@@ -20,164 +23,167 @@ row2                  [6,7]
 
 ]
 
-Tips: use navigateMatrix helper to navigate in this data structure: navigateMatrix <pageNumber> <rowNumber>
+Tip:
+Use `navigateMatrix` helper to navigate in this data structure:
+- navigateMatrix <pageNumber> <rowNumber>
 
-* */
+**/
+
 function paginator(notice, withBollettinoPoste) {
+  const instalments = notice.instalments.items;
+  const instalmentsNumber = instalments.length;
 
-    let instalments = notice.instalments.items;
-    const instalmentsLength = instalments.length;
-    let data = {};
-    if (withBollettinoPoste) {
-        data = getPaginationDataPoste(instalmentsLength);
+  const data = withBollettinoPoste ? getPaginationDataPoste(instalmentsNumber) : getPaginationData(instalmentsNumber);
 
-    } else {
-        data = getPaginationData(instalmentsLength);
-    }
-
-    return paginateAndGroupArray(instalments, data.firstPageRows, data.firstPageColumns, data.secondPageRows, data.secondPageColumns);
+  return paginateAndGroupArray(
+    instalments,
+    data?.firstPageRows,
+    data?.firstPageColumns,
+    data?.secondPageRows,
+    data?.secondPageColumns
+  );
 }
 
 function paginateAndGroupArray(array, firstPageRows, firstPageCols, pageRows, pageCols) {
-    // Funzione per dividere una pagina in righe e colonne
-    function splitIntoRowsAndCols(page, numRows, numCols) {
-        let rows = [];
-        for (let i = 0; i < numRows; i++) {
-            let start = i * numCols;
-            let end = start + numCols;
-            if (start < page.length) {
-                rows.push(page.slice(start, end));
-            }
-        }
-        return rows;
+  /* Function to divide a page in rows and columns */
+  function splitIntoRowsAndCols(page, numRows, numCols) {
+    let rows = [];
+    for (let i = 0; i < numRows; i++) {
+      let start = i * numCols;
+      let end = start + numCols;
+      if (start < page.length) {
+        rows.push(page.slice(start, end));
+      }
     }
+    return rows;
+  }
 
-    // Risultato finale, un array di pagine, ogni pagina è un array di righe
-    let paginatedArray = [];
+  /* Final output:
+  - Array of pages, every page is an array of rows
+  */
+  let paginatedArray = [];
 
-    // Aggiungi la prima pagina con un numero di righe e colonne specifico
-    let firstPageSize = firstPageRows * firstPageCols;
-    let firstPage = array.slice(0, firstPageSize);
-    paginatedArray.push(splitIntoRowsAndCols(firstPage, firstPageRows, firstPageCols));
+  /* Add the first page with a specific number of rows and columns */
+  const firstPageSize = firstPageRows * firstPageCols;
+  const firstPage = array.slice(0, firstPageSize);
+  paginatedArray.push(splitIntoRowsAndCols(firstPage, firstPageRows, firstPageCols));
+  /* Calculate the number of remaining items */
+  const remainingItems = array.length - firstPageSize;
 
-    // Calcola il numero di elementi rimanenti
-    let remainingItems = array.length - firstPageSize;
+  /* Divide the remaining items of the array in pages */
+  const pageSize = pageRows * pageCols;
+  const totalPages = Math.ceil(remainingItems / pageSize);
 
-    // Divide il resto dell'array in pagine
-    let pageSize = pageRows * pageCols;
-    let totalPages = Math.ceil(remainingItems / pageSize);
+  for (let i = 0; i < totalPages; i++) {
+    const start = firstPageSize + i * pageSize;
+    const end = start + pageSize;
+    const page = array.slice(start, end);
+    paginatedArray.push(splitIntoRowsAndCols(page, pageRows, pageCols));
+  }
 
-    for (let i = 0; i < totalPages; i++) {
-        let start = firstPageSize + i * pageSize;
-        let end = start + pageSize;
-        let page = array.slice(start, end);
-        paginatedArray.push(splitIntoRowsAndCols(page, pageRows, pageCols));
-    }
-
-    return paginatedArray;
+  return paginatedArray;
 }
 
-function getPaginationDataPoste(instalmentsLength) {
-    let firstPageRows = 0;
-    let firstPageColumns = 0;
-    let secondPageRows = 0;
-    let secondPageColumns = 0;
+function getPaginationDataPoste(instalmentsNumber) {
+  let firstPageRows = 0;
+  let firstPageColumns = 0;
+  let secondPageRows = 0;
+  let secondPageColumns = 0;
 
-    switch (instalmentsLength) {
-        case 2:
-            firstPageRows = 1;
-            firstPageColumns = 2;
-            break;
-        case 3:
-            firstPageRows = 1;
-            firstPageColumns = 3;
-            break;
-        case 4:
-            firstPageRows = 1;
-            firstPageColumns = 2;
-            secondPageRows = 1;
-            secondPageColumns = 2;
-            break;
-        case 5:
-            firstPageRows = 1;
-            firstPageColumns = 2;
-            secondPageRows = 1;
-            secondPageColumns = 3;
-            break;
-        case 6:
-            firstPageRows = 1;
-            firstPageColumns = 3;
-            secondPageRows = 1;
-            secondPageColumns = 3;
-            break;
-        case 7:
-            firstPageRows = 1;
-            firstPageColumns = 3;
-            secondPageRows = 1;
-            secondPageColumns = 2;
-            break;
-        case 8:
-            firstPageRows = 1;
-            firstPageColumns = 2;
-            secondPageRows = 1;
-            secondPageColumns = 3;
-            break;
-        default:
-            firstPageRows = 1;
-            firstPageColumns = 3;
-            secondPageRows = 1;
-            secondPageColumns = 3;
-    }
-    return {firstPageRows, firstPageColumns, secondPageRows, secondPageColumns};
+  switch (instalmentsNumber) {
+    case 2:
+      firstPageRows = 1;
+      firstPageColumns = 2;
+      break;
+    case 3:
+      firstPageRows = 1;
+      firstPageColumns = 3;
+      break;
+    case 4:
+      firstPageRows = 1;
+      firstPageColumns = 2;
+      secondPageRows = 1;
+      secondPageColumns = 2;
+      break;
+    case 5:
+      firstPageRows = 1;
+      firstPageColumns = 2;
+      secondPageRows = 1;
+      secondPageColumns = 3;
+      break;
+    case 6:
+      firstPageRows = 1;
+      firstPageColumns = 3;
+      secondPageRows = 1;
+      secondPageColumns = 3;
+      break;
+    case 7:
+      firstPageRows = 1;
+      firstPageColumns = 3;
+      secondPageRows = 1;
+      secondPageColumns = 2;
+      break;
+    case 8:
+      firstPageRows = 1;
+      firstPageColumns = 2;
+      secondPageRows = 1;
+      secondPageColumns = 3;
+      break;
+    default:
+      firstPageRows = 1;
+      firstPageColumns = 3;
+      secondPageRows = 1;
+      secondPageColumns = 3;
+  }
+  return { firstPageRows, firstPageColumns, secondPageRows, secondPageColumns };
 }
 
-function getPaginationData(instalmentsLength) {
-    const firstPageRows = 1;
-    let firstPageColumns = 2;
-    let secondPageRows = 3;
-    let secondPageColumns = 2;
+function getPaginationData(instalmentsNumber) {
+  const firstPageRows = 1;
+  let firstPageColumns = 2;
+  let secondPageRows = 3;
+  let secondPageColumns = 2;
 
-    switch (instalmentsLength) {
-        case 2:
-            firstPageColumns = 2;
-            secondPageRows = 0;
-            break;
-        case 3:
-            firstPageColumns = 3;
-            secondPageRows = 0;
-            break;
-        case 4:
-            firstPageColumns = 2;
-            secondPageRows = 1;
-            secondPageColumns = 2;
-            break;
-        case 5:
-            firstPageColumns = 2;
-            secondPageRows = 1;
-            secondPageColumns = 3;
-            break;
-        case 6:
-            firstPageColumns = 2;
-            secondPageRows = 2;
-            secondPageColumns = 2;
-            break;
-        case 7:
-            firstPageColumns = 3;
-            secondPageRows = 2;
-            secondPageColumns = 2;
-            break;
-        case 8:
-            firstPageColumns = 2;
-            secondPageRows = 2;
-            secondPageColumns = 3;
-            break;
-        default:
-            firstPageColumns = 3;
-            secondPageRows = 3;
-            secondPageColumns = 3;
-    }
-    return {firstPageRows, firstPageColumns, secondPageRows, secondPageColumns};
+  switch (instalmentsNumber) {
+    case 2:
+      firstPageColumns = 2;
+      secondPageRows = 0;
+      break;
+    case 3:
+      firstPageColumns = 3;
+      secondPageRows = 0;
+      break;
+    case 4:
+      firstPageColumns = 2;
+      secondPageRows = 1;
+      secondPageColumns = 2;
+      break;
+    case 5:
+      firstPageColumns = 2;
+      secondPageRows = 1;
+      secondPageColumns = 3;
+      break;
+    case 6:
+      firstPageColumns = 2;
+      secondPageRows = 2;
+      secondPageColumns = 2;
+      break;
+    case 7:
+      firstPageColumns = 3;
+      secondPageRows = 2;
+      secondPageColumns = 2;
+      break;
+    case 8:
+      firstPageColumns = 2;
+      secondPageRows = 2;
+      secondPageColumns = 3;
+      break;
+    default:
+      firstPageColumns = 3;
+      secondPageRows = 3;
+      secondPageColumns = 3;
+  }
+  return { firstPageRows, firstPageColumns, secondPageRows, secondPageColumns };
 }
-
 
 module.exports = paginator;
