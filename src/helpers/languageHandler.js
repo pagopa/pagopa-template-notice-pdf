efcfaad0e3c65bc40f448aa49ec175ede59d168e
Damiano Plebani <damiano.plebani@pagopa.it>
2024-10-01 11:07:41 +0200

    Add support for custom variables to the `languageHandler`

--- src/helpers/languageHandler.js
+++ src/helpers/languageHandler.js
@@ -3,13 +3,19 @@ const i18next = require("i18next");
 function languageHandler(i18n_key, options) {
   /* Get language parameters from JSON file */
   const languageInfo = options.data.root.metadata;
-  const textBoxAlignment = options.hash.align ?? "horizontal";
-  const isTrueBilingual = languageInfo?.trueBilingualism;
   const mainLanguage = languageInfo?.language;
   const secondaryLanguage = languageInfo?.secondaryLanguage;
+  const isTrueBilingual = languageInfo?.trueBilingualism;
+
+  /* Get parameters from the helper */
+  const { align: textBoxAlignment = "horizontal", forceSecondaryLang, ...variables } = options.hash;
 
   /* Get localized text using i18n key */
-  const mainLocalizedText = i18next.t(i18n_key, { lng: mainLanguage ?? "it" });
+  const mainLocalizedText = i18next.t(i18n_key, {
+    lng: mainLanguage ?? "it",
+    interpolation: { escapeValue: false },
+    ...variables,
+  });
 
   /* If there are no hash parameters, only the main localised text
   will be returned. This could be used to render
@@ -19,11 +25,20 @@ function languageHandler(i18n_key, options) {
   }
 
   /* Create the mainHTML element with default language text */
-  const mainHTMLElem = '<p class="' + options.hash.classPrimaryLang + '">' + mainLocalizedText + "</p>";
+  const mainHTMLElem = `<p class="${options.hash.classPrimaryLang}">${mainLocalizedText}</p>`;
 
   /* Create the secondaryHTML element, using the secondary language */
   if (mainLanguage && secondaryLanguage) {
-    const secondaryLocalizedText = i18next.t(i18n_key, { lng: languageInfo.secondaryLanguage });
+    const secondaryLocalizedText = i18next.t(i18n_key, {
+      lng: languageInfo.secondaryLanguage,
+      interpolation: { escapeValue: false },
+      ...variables,
+    });
+
+    if (forceSecondaryLang) {
+      return `<p class="${options.hash.classPrimaryLang}">${secondaryLocalizedText}</p>`;
+    }
+
     const secondaryElementClassname = isTrueBilingual ? options.hash.classBilingual : options.hash.classSecondaryLang;
     const secondaryHTMLElem =
       !isTrueBilingual && textBoxAlignment === "horizontal"
