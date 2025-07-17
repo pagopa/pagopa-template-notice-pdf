e3367f178957ca07c7ac88e6a04104f6a3fe5300
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-07-17 12:38:58 +0200

    fix: using bot token stored in domain's KeyVault

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -48,7 +48,7 @@ data "azurerm_key_vault_secret" "key_vault_sonar" {
 
 data "azurerm_key_vault_secret" "key_vault_bot_token" {
   name         = "bot-token-github"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
